2dde90e7c5e9ef7fa8080a6d2b10db6ed3e4cd3a
minium2 <33176405+minium2@users.noreply.github.com>
2021-10-26 19:43:09 +0300

    change main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
-//
-// Created by User on 10.10.2021.
-//
+#include <iostream>
+#include "Sexpression.h"
+#include <SDocument.h>
+
+int main() {
+    std::string str("(hi there (what a cool (little list) parser) (library))");
+
+    auto s = SDocument::parse(str);
+    s["hi"]->addChild("str");
+    std::cout << s.toString() << std::endl;
+
+    return 0;
+}
 
